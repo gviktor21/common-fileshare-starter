cc10b64a2817cb3e4491c2a8179fedd2bda08277
GViktor <godov2@gmail.com>
2022-02-17 11:58:46 +0100

    Image file added and an initial package structure.

---
+++ src/main/java/com/codecool/fileshare/AdminConsolApp/ConsolMain.java
@@ -0,0 +1,10 @@
+package com.codecool.fileshare.AdminConsolApp;
+
+public class ConsolMain {
+    /**
+     * Call this from FileshareApplication if console is needed
+     * */
+    public static void ConsoleMain(){
+
+    }
+}
--- src/main/java/com/codecool/fileshare/FileshareApplication.java
+++ src/main/java/com/codecool/fileshare/FileshareApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FileshareApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileshareApplication.class, args);
+        System.out.println("Jozso");
     }
 }
